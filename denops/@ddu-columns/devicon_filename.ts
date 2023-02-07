7dd617116c3d8ff37b8e1cdcbd15ccd353594953
tamago3keran <yamataku3831@gmail.com>
2023-02-07 02:37:16 +0000

    experimentally commit

---
+++ denops/@ddu-columns/devicon_filename.ts
@@ -0,0 +1,129 @@
+import {
+  BaseColumn, DduItem,
+  ItemHighlight,
+} from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+import { GetTextResult } from "https://deno.land/x/ddu_vim@v2.0.0/base/column.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
+import { basename } from "https://deno.land/std@0.165.0/path/mod.ts";
+import { getDevicon, setDeviconColor } from "./devicon_data.ts";
+
+type Params = {
+  collapsedIcon: string;
+  expandedIcon: string;
+  iconWidth: number;
+  linkIcon: string;
+  highlights: HighlightGroup;
+};
+
+type HighlightGroup = {
+  directoryIcon?: string;
+  directoryName?: string; linkIcon?: string;
+  linkName?: string;
+};
+
+type ActionData = {
+  isDirectory?: boolean;
+  isLink?: boolean;
+  path?: string;
+};
+
+export class Column extends BaseColumn<Params> {
+  override async getLength(args: {
+    denops: Denops;
+    columnParams: Params;
+    items: DduItem[];
+  }): Promise<number> {
+    const widths = await Promise.all(args.items.map(
+      async (item) =>
+        item.__level + 1 +
+        (await fn.strwidth(
+          args.denops,
+          args.columnParams.iconWidth + (item.display ?? item.word),
+        ) as number),
+    )) as number[];
+    return Math.max(...widths);
+  }
+override async getText(args: { denops: Denops; columnParams: Params;
+    startCol: number;
+    endCol: number;
+    item: DduItem;
+  }): Promise<GetTextResult> {
+    const action = args.item?.action as ActionData;
+    const highlights: ItemHighlight[] = [];
+    const isDirectory = args.item.isTree ?? false;
+    const isLink = action.isLink ?? false;
+    const path = basename(action.path ?? args.item.word) +
+      (isDirectory ? "/" : "");
+
+    if (isDirectory) {
+      const userHighlights = args.columnParams.highlights;
+      highlights.push({
+        name: "column-filename-directory-icon",
+        "hl_group": userHighlights.directoryIcon ?? "Special",
+        col: args.startCol + args.item.__level,
+        width: args.columnParams.iconWidth,
+      });
+
+      highlights.push({
+        name: "column-filename-directory-name",
+        "hl_group": userHighlights.directoryName ?? "Directory",
+        col: args.startCol + args.item.__level +
+          args.columnParams.iconWidth + 1,
+        width: path.length,
+      });
+    } else if (isLink) {
+      const userHighlights = args.columnParams.highlights;
+      highlights.push({
+        name: "column-filename-link-icon",
+        "hl_group": userHighlights.linkIcon ?? "Comment",
+        col: args.startCol + args.item.__level,
+        width: args.columnParams.iconWidth,
+      });
+
+      highlights.push({
+        name: "column-filename-link-name",
+        "hl_group": userHighlights.linkName ?? "Comment",
+        col: args.startCol + args.item.__level +
+          args.columnParams.iconWidth + 1,
+        width: path.length,
+      });
+    }
+
+    const directoryIcon = args.item.__expanded
+      ? args.columnParams.expandedIcon
+      : args.columnParams.collapsedIcon;
+    const icon = isDirectory
+      ? directoryIcon
+      : isLink
+      ? args.columnParams.linkIcon
+      : getDevicon({ fileName: path });
+    const text = " ".repeat(args.item.__level) + icon + " " + path;
+    const width = await fn.strwidth(args.denops, text) as number;
+    const padding = " ".repeat(args.endCol - args.startCol - width);
+
+    if (!isDirectory && !isLink) {
+      setDeviconColor({
+        denops: args.denops,
+        fileName: path,
+        highlights: highlights,
+        col: args.startCol + args.item.__level,
+        width: args.columnParams.iconWidth,
+      });
+    }
+
+    return Promise.resolve({
+      text: text + padding,
+      highlights: highlights,
+    });
+  }
+
+  override params(): Params {
+    return {
+      collapsedIcon: "",
+      expandedIcon: "",
+      iconWidth: 3,
+      linkIcon: "@",
+      highlights: {},
+    };
+  }
+}
